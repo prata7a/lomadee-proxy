160f18a6d28a8f9051cebfd3c4996f581cdae31c
prata7a <umbrenno@gmail.com>
2025-09-16 11:55:25 -0300

    Update coupons.js

--- api/coupons.js
+++ api/coupons.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const API_KEY = "HpV0rIXRuTIhta7k0MPZAqLkO14Q9Vsr"; // sua API Key da Lomadee
- const SOURCE_ID = "9125b777-4886-40e7-a874-86e2471ca3a7"; // ID real do canal
+  const SOURCE_ID = "9125b777-4886-40e7-a874-86e2471ca3a7"; // ID do canal (correto)
 
   try {
     const lomadeeRes = await fetch(
@@ -12,10 +12,10 @@ export default async function handler(req, res) {
     }
 
     const data = await lomadeeRes.json();
-
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error: "Erro interno no proxy", details: error.message });
   }
 }
+
